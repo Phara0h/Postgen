f2598eac3ee85f44238c474ae558c67736ad7047
Jt Whissel <jtwhissel@gmail.com>
2019-08-22 12:30:40 -0400

    Postgen's init commit

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # Postgen
+
 A simple node script to convert postman collections to clean REST client libs for node.
+
+## Install
+
+```
+npm install -g postgen
+```
+
+## How to use
+
+* Export your collection from postman as a `v2.1` collection.
+* `cd` into your projects root folder that you want to use the client with.
+* Install `fasquest` as it is the only dependency of the generated client.
+```
+npm install --save fasquest
+```
+* Run postgen
+```
+postgen /path/to/postman/collection.js > YourApi.js
+```
+* View the examples on how to use your newly generated lib.
+
+## Examples
+
+### [Wasps with Bazookas](https://github.com/Phara0h/WaspsWithBazookas) Service
+
+A postman collection of the service:
+ <img src="https://i.imgur.com/SssAdP5.png" alt="https://www.npmjs.com/package/waspswithbazookas" data-canonical-src="https://i.imgur.com/SssAdP5.png" />
+
+Generated a class structure:
+```
+WaspsWithBazookas
+    |
+    |___Hive
+    |___Wasps
+    |___Wasp
+```
+
+#### Example use of the generation.
+```js
+
+const WaspsWithBazookas = require('./WaspsWithBazookasAPI.js')(
+  'https://127.0.0.1:4269' // the url to service with no trailing /
+);
+
+// Start loadtest for 1second
+WWB.Hive.HivePoke({
+  "t": "10",
+  "c": "50",
+  "d": "1",
+  "target": "http://127.0.0.1:4269/hive/status"
+}).then(response=>{
+  console.log(response.body)
+})
+
+// Wait 2 seconds and show the report
+setTimeout(async ()=>{
+  console.log((await WWB.Hive.HiveStatusReport()).body)
+},2000)
+
+```
+
+
+### [Travelling](https://github.com/abeai/travelling) Service
+
+A postman collection of the service:
+ <img src="https://i.imgur.com/2hD4oPU.png" alt="https://www.npmjs.com/package/waspswithbazookas" data-canonical-src="https://i.imgur.com/2hD4oPU.png" />
+
+Generated a class structure:
+```
+Travelling
+    |
+    |___Auth
+    |___Groups
+    |___User
+         |____Current
+```
+
+#### Example use of the generation.
+```js
+
+const Travelling = require('./TravellingAPI.js')(
+  'https://127.0.0.1:6969' // the url to service with no trailing /
+);
+
+Travelling.Auth.login({
+  "username":"test",
+  "password":"password1234"
+}).then(response=>{
+  console.log(response.body)
+})
+
+```
+
+
+
+## Further Development
+The code to do the generation is just some hacky code busted out in a hour. I would love help cleaning it up and making it more feature rich.
---
+++ index.js
@@ -0,0 +1,190 @@
+#!/usr/bin/env node
+
+const fs = require('fs') // needed to read JSON file from disk
+const Collection = require('postman-collection').Collection;
+const beautify = require('beautify');
+
+// Load a collection to memory from a JSON file on disk (say, sample-collection.json)
+var collection = new Collection(JSON.parse(fs.readFileSync(process.argv[2]).toString())).toJSON();
+var allNewClasses = [];
+
+var jsFile = `
+const fasq = require('fasquest');
+var hostUrl = '';
+`
+var parseHeader = function (header)
+{
+  var params = '';
+  for (var i = 0; i < header.length; i++)
+  {
+    if (header[i].key == 'Content-Type' && header[i].value == 'application/json')
+    {
+      params += 'json:true'
+    }
+  }
+
+  return params;
+}
+
+var parseUrl = function (url)
+{
+  for (var i = 0; i < url.path.length; i++)
+  {
+    if (url.path[i].indexOf(':') > -1)
+    {
+      url.path[i] = '${' + url.path[i].replace(':', '') + '}';
+    }
+  }
+
+  var uri = url.path.join('/')
+  var params = `uri: hostUrl+"/" + ` + '`' + uri + '`'
+
+
+  if (url.query.length > 0)
+  {
+    params += ', qs: { '
+    for (var i = 0; i < url.query.length; i++)
+    {
+      params += i + 1 < url.query.length ? url.query[i].key + ',' : url.query[i].key;
+    }
+    params += '}'
+  }
+  return params;
+}
+
+var getVars = function (url, body)
+{
+  var vars = [...Array.from(url.variable.map(v => v.key)), ...Array.from(url.query.map(v => v.key))].join();
+
+  return (body ? 'body' + (vars.length > 0 ? ',' : '') : '') + vars;
+}
+//
+var convertToOptions = function (request)
+{
+  var options = `{
+    method: '${request.method}', resolveWithFullResponse:true, simple: false, ${parseUrl(request.url)}, ${request.body ? 'body,' : ''} ${request.header ? parseHeader(request.header) + ',' : ''}
+  }`;
+
+  return options;
+}
+
+
+var setMethodName = function (n)
+{
+  var mn = '' + n
+  return toCamel(mn.replace(/:/g, '').replace(/[ \/-]/g, '_').toLowerCase())
+}
+
+var setClassName = function (n)
+{
+  var cn = '' + n;
+  var name = toCamel(cn.replace(/ /g, '_').toLowerCase());
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+const toCamel = (s) =>
+{
+  return s.replace(/([-_][a-z])/ig, ($1) =>
+  {
+    return $1.toUpperCase()
+      .replace('-', '')
+      .replace('_', '');
+  });
+};
+
+const isArray = function (a)
+{
+  return Array.isArray(a);
+};
+
+const isObject = function (o)
+{
+  return o === Object(o) && !isArray(o) && typeof o !== 'function';
+};
+
+const keysToCamel = function (o)
+{
+  if (isObject(o))
+  {
+    const n = {};
+
+    Object.keys(o)
+      .forEach((k) =>
+      {
+        n[toCamel(k)] = keysToCamel(o[k]);
+      });
+
+    return n;
+  }
+  else if (isArray(o))
+  {
+    return o.map((i) =>
+    {
+      return keysToCamel(i);
+    });
+  }
+
+  return o;
+};
+
+var genClass = function (name, item, js)
+{
+
+  var classEnd = false;
+  //allNewClasses.push(setClassName(name));
+  var newClasses = [];
+  js += `class ${setClassName(name)} {
+    constructor() {
+    }
+    `
+  for (var i = item.length - 1; i >= 0; i--)
+  {
+
+    if (!item[i].item)
+    {
+      js += `
+          static async ${setMethodName(item[i].name)}(${getVars(item[i].request.url, !!item[i].request.body)}) {
+              var options = ${convertToOptions(item[i].request)};
+              return await fasq.request(options)
+          }
+      `;
+    }
+    else
+    {
+      if (!item[i].name)
+      {
+        console.log(item[i])
+      }
+      js += `
+        static get ${setClassName(item[i].name)}() {
+          return ${setClassName(item[i].name)};
+        }
+      `
+      newClasses.push(item[i]);
+
+    }
+  }
+  js += `}`
+
+  for (var i = 0; i < newClasses.length; i++)
+  {
+    js += genClass(newClasses[i].name, newClasses[i].item, '');
+  }
+
+  return js;
+
+}
+
+jsFile += genClass(collection.info.name, collection.item, '')
+
+jsFile += `
+  module.exports= function(host){
+    hostUrl = host;
+
+    return ${setClassName(collection.info.name)};
+  }
+`
+//allNewClasses.join()
+console.log(beautify(jsFile,
+{
+  format: 'js'
+}));
